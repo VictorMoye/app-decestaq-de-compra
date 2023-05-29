70a9a5a453bf95aa60e3d9fa4f3091eff2e5e022
victor moye <51638222+VictorMoye@users.noreply.github.com>
2023-05-29 17:40:53 -0300

    finalizando a imagens do app

--- src/Telas/Cestas.js
+++ src/Telas/Cestas.js
@@ -1,6 +1,28 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { StyleSheet,Image, Dimensions, Text } from 'react-native';
 import topo from '../../assets/topo.png'
+
+const width = Dimensions.get('screen').width
+
 export default function Cestas(){
-    return <Image source={topo} />
+    return <>    
+    <Image source={topo}  style = {estilos.topo}/>
+    <Text style= {estilos.titulo }>Detalhe da Cestas</Text>
+    </> 
 }
+
+const estilos =  StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: 578 / 768 * width,
+},
+    titulo: {
+        position: "Absolute", 
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight: 26,
+        color: "white",
+        fontWeight: "bold",
+        padding: 16,
+    },
+});
