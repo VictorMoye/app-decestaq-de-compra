c5a3678b72ccb700774018374e5859fc13711429
victor moye <51638222+VictorMoye@users.noreply.github.com>
2023-05-30 23:17:59 -0300

    usando o flat e finalizando o front end do projeto

--- App.js
+++ App.js
@@ -1,23 +1,25 @@
 
-import { StatusBar, View, SafeAreaView } from 'react-native';
+import { StatusBar,  SafeAreaView } from 'react-native';
 import { useFonts, 
   Montserrat_400Regular,
   Montserrat_700Bold} from '@expo-google-fonts/montserrat';
 import AppLoading from 'expo-app-loading';
-import Cestas from './src/telas/Cesta/';
+import Cesta from './src/telas/Cesta/';
 import mock from './src/mocks/cestas';
 export default function App() {
   const [fonteCarregada] = useFonts({
     "MontserratRegular": Montserrat_400Regular,
-    'MontserratBold': Montserrat_700Bold,
-  }) ;
-  if(!fonteCarregada){
-    return <AppLoading/>
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if (!fonteCarregada) {
+    return <AppLoading />
   }
+
   return (
-    <View>
+    <SafeAreaView style={{ flex: 1 }}>
       <StatusBar />
-      <Cestas {...mock}/>      
-    </View>
+      <Cesta {...mock} />
+    </SafeAreaView>
   );
 }
